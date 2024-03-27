6506d55212269d0d978f02448a1286c767f02864
meo-young <meoyoung000@naver.com>
2024-03-27 13:17:13 +0900

    Feat Scroll

--- app/src/main/java/com/example/week04/mainscreen.kt
+++ app/src/main/java/com/example/week04/mainscreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Badge
@@ -96,10 +98,15 @@ fun MainScreen(){
     var counter3 by remember {
         mutableStateOf(0)
     }
+    var counter4 by remember {
+        mutableStateOf(0)
+    }
+
+    val scrollState = rememberScrollState()
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize().verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         ImageWithSlot(img = R.drawable.img1) {
             ButtonWithIcon(counter = counter1) {
@@ -114,6 +121,11 @@ fun MainScreen(){
         ImageWithSlot(img = R.drawable.img3) {
             ButtonWithIcon(counter = counter3) {
                 counter3++
+            }
+        }
+        ImageWithSlot(img = R.drawable.img3) {
+            ButtonWithIcon(counter = counter4) {
+                counter4++
             }
         }
     }
